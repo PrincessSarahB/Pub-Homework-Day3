9be342283a4ac442cb677d8cf20c870451f0cde1
Hadsan Geele <hgeele22@gmail.com>
2018-04-04 12:26:23 +0100

    customer tests passing

---
+++ customer.rb
@@ -0,0 +1,23 @@
+class Customer
+
+def initialize(customer_name , wallet)
+
+@customer_name = customer_name
+@wallet = wallet
+end
+
+def customer_name()
+
+  return @customer_name
+
+end
+
+def wallet_contents()
+return @wallet
+end
+
+
+
+
+
+end
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -1,14 +1,25 @@
-# require("minitest/autorun")
-# require('minitest/rg')
-# require_relative("../pub.rb")
-# require_relative("../drink.rb")
-# require_relative("../customer.rb")
-#
-#
-# class CustomerTest < MiniTest::Test
-#
-# def setup()
-#
-# @ =
-#
-# end
+require("minitest/autorun")
+require('minitest/rg')
+require_relative("../pub.rb")
+require_relative("../drink.rb")
+require_relative("../customer.rb")
+
+
+class CustomerTest < MiniTest::Test
+
+def setup()
+
+@customer = Customer.new("Bob", 50.00)
+
+end
+
+def test_customer_name()
+
+assert_equal("Bob" , @customer.customer_name)
+end
+
+def test_wallet_contents()
+
+assert_equal(50.00 , @customer.wallet_contents)
+end
+end
